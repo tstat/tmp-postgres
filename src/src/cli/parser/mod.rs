3e1206122423bb14fecd6d515dc77218644b2ee0
Travis Staton <tstaton@mercury.com>
2024-07-25 21:23:40 -0400

    Document command line options

--- src/src/cli/parser/mod.rs
+++ src/src/cli/parser/mod.rs
@@ -2,18 +2,28 @@ use clap::Parser;
 use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
+#[command(version, about)]
 pub struct Args {
+    /// The directory to initialize a db cluster in
     pub directory: PathBuf,
+    /// Run postgres in an existing db cluster
     #[arg(long)]
     pub use_existing_dir: bool,
+    /// Localhost port to listen on
     #[arg(short, long)]
     pub port: Option<u16>,
+    /// Control if the directory is deleted when tmp-postgres ends
     #[arg(long = "remove")]
     pub should_remove: Option<bool>,
+    /// Start a psql session
     #[arg(long)]
     pub psql: bool,
+    /// Silences stdout/stderr forwarding from initdb and postgres
     #[arg(long)]
     pub silent: bool,
+    /// Optional command to run when tmp-postgres is ready to receive
+    /// connections, when specified tmp-postgres will tear down after the
+    /// command exits
     #[arg(trailing_var_arg = true)]
     pub around: Vec<String>,
 }
